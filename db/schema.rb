8805af0f403aafbdaa0f32a27cfa446f45583120
jtb1137 <jtb1137@gmail.com>
2018-02-27 16:47:11 -0500

    generated restaurant_categories join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227212212) do
+ActiveRecord::Schema.define(version: 20180227214553) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurant_categories", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
