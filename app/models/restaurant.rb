c030225c14db144952acf19bc5491a61dd40316c
jtb1137 <jtb1137@gmail.com>
2018-02-27 16:11:35 -0500

    image visible on show view

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
     validates_presence_of :name, presence: :true
-    has_attached_file :restaurant_img, styles: { medium: "300x300>", thumb: "286x180>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :restaurant_img, styles: { medium: "1144x720>", thumb: "286x180>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :restaurant_img, content_type: /\Aimage\/.*\z/
 end
