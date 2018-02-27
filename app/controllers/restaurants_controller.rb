d45ffa191396e3e16fcf9abc8eb24286264ae179
jtb1137 <jtb1137@gmail.com>
2018-02-27 13:16:16 -0500

    outline restaurants controller and add routes

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,27 @@
+class RestaurantsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def restaurant_params
+    end
+end
