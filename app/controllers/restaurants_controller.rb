5555e30c41170dbf4f6fd23fed8e78e73543d537
jtb1137 <jtb1137@gmail.com>
2018-02-27 13:44:33 -0500

    create and view index of restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,22 @@
 class RestaurantsController < ApplicationController
     def index
+        @restaurants = Restaurant.all
     end
 
     def new
+        @restaurant = Restaurant.new
     end
 
     def create
+        @restaurant = Restaurant.new(restaurant_params)
+        # don't forget to set values for associated models when they are added
+        if @restaurant.save
+            flash[:alert] = "Successfully Saved Restaurant"
+            redirect_to root_path # Change this
+        else
+            flash[:alert] = "Failed to Save Restaurant"
+            render 'new'
+        end
     end
 
     def show
@@ -23,5 +34,6 @@ class RestaurantsController < ApplicationController
     private
 
     def restaurant_params
+        params.require(:restaurant).permit(:name, :address)
     end
 end
