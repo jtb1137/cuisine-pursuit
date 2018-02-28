da8f10e792d51565bc8b0a42d9f2d674882dc6cc
jtb1137 <jtb1137@gmail.com>
2018-02-28 08:55:48 -0500

    customize categories_attributes= and update form and show views

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,10 @@ class Restaurant < ApplicationRecord
     has_many :categories, through: :restaurant_categories
     accepts_nested_attributes_for :categories
 
+    def categories_attributes=(category_attributes)
+        category_attributes.values.each do |category_attribute|
+            new_category = Category.find_or_create_by(category_attribute)
+            self.categories << new_category
+        end
+    end
 end
